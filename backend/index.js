5be999239e4874111d5955e8bf95a7af98b0e2b7
Amir <rahavamir1@gmail.com>
2020-07-27 13:35:36 +0300

    adding mlab

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
 const cors = require('cors')
 
+const PORT = process.env.PORT || 5000
 const Player = require('./model/playersSchema')
 require('dotenv').config()
-const PORT = process.env.PORT || 5000
 
 const app = express();
 app.use(cors())
@@ -24,7 +24,6 @@ app.get('/', (req, res) => {
         res.json(doc)
     })
 
-    res.json();
 })
 
 
